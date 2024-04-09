2a024da417b2aa04444d1d4f754b65c33267981a
elyandreb <143809194+elyandreb@users.noreply.github.com>
2024-04-09 13:22:16 +0000

    création des méthodes

--- src/Equipe.java
+++ src/Equipe.java
@@ -1,3 +1,32 @@
-public class Equipe {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Equipe implements Participant{
+    private String nom;
+    private List<Athlète> listAthlètes;
+
+    public Equipe(String nom, List<Athlète> listAthlètes){
+        this.nom = nom;
+        this.listAthlètes = new ArrayList<>();
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public List<Athlète> getMembres(){
+        return this.listAthlètes;
+    }
+
+    @Override
+    public void participer(Epreuve epreuve){
+        
+    }
+
+    @Override
+    public String toString(){
+        return ("nom de l'équipe : " + this.getNom() + "; liste des athlètes de l'équipe : " + this.getMembres());
+    }
+
 }
+
