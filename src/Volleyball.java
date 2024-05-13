55d34a1b358434167fd23f097db9c6167777b8b0
farid-cheddad <143810134+farid-cheddad@users.noreply.github.com>
2024-05-13 11:42:44 +0200

    Mise à jour de la classe Volleyball

--- src/Volleyball.java
+++ src/Volleyball.java
@@ -1,6 +1,17 @@
-public class Volleyball extends Sport{
+/**
+ * La classe Volleyball représente le sport de volley-ball dans le contexte des Jeux Olympiques
+ * Elle hérite de la classe abstraite Sport
+ */
+public class Volleyball extends Sport {
     
-    public Volleyball(String nom,String categorie,int nbjoueur){
+    /**
+     * Constructeur de la classe Volleyball
+     * @param nom Le nom du sport (ici, "Volleyball")
+     * @param categorie La catégorie du sport (individuel ou collectif)
+     * @param nbjoueur Le nombre de joueurs dans une équipe de volley-ball
+     */
+    public Volleyball(String nom, String categorie, int nbjoueur) {
         super(nom, categorie, nbjoueur);
     }
+    
 }
