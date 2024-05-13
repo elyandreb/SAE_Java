17322f1b5a5bad41b0ba52c1b37f8ef942d57cb2
farid-cheddad <143810134+farid-cheddad@users.noreply.github.com>
2024-05-13 11:39:52 +0200

    Mise à jour de la classe Handball

--- src/Handball.java
+++ src/Handball.java
@@ -1,6 +1,17 @@
-public class Handball extends Sport{
-    
-    public Handball(String nom,String categorie,int nbjoueur){
+/**
+ * La classe Handball représente le sport de handball dans le contexte des Jeux Olympiques
+ * Elle hérite de la classe abstraite Sport
+ */
+public class Handball extends Sport {
+
+    /**
+     * Constructeur de la classe Handball
+     * @param nom Le nom du sport (ici, "Handball")
+     * @param categorie La catégorie du sport (individuel ou collectif)
+     * @param nbjoueur Le nombre de joueurs dans une équipe de handball
+     */
+    public Handball(String nom, String categorie, int nbjoueur) {
         super(nom, categorie, nbjoueur);
     }
+
 }
