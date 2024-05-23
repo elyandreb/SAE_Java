a761e1bfe1ab7a79bd5ba6e77121bf52da0a4a3f
farid-cheddad <143810134+farid-cheddad@users.noreply.github.com>
2024-05-23 18:43:41 +0200

    Create TriMedaillesTotalDecroissantPays.java

---
+++ src/TriMedaillesTotalDecroissantPays.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+
+/**
+ * Cette classe implémente l'interface Comparator pour trier les objets Pays
+ * en fonction du nombre total de médailles dans l'ordre décroissant
+ */
+public class TriMedaillesTotalDecroissantPays implements Comparator<Pays> {
+
+    /**
+     * Compare deux objets Pays en fonction de leur nombre total de médailles
+     * @param p1 Le premier objet Pays à comparer
+     * @param p2 Le deuxième objet Pays à comparer
+     * @return Un entier négatif si p2 a moins de médailles que p1
+     *         un entier positif si p2 a plus de médailles que p1
+     *         et zéro si les deux ont le même nombre de médailles
+     */
+    @Override
+    public int compare(Pays p1, Pays p2) {
+        int nbMedailles1 = p1.nbMedaillesTotal();
+        int nbMedailles2 = p2.nbMedaillesTotal();
+        return nbMedailles2 - nbMedailles1; // Ordre décroissant
+    }
+    
+}
