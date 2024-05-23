ed9c34a052c9d640efa592f0794a3ad156679952
farid-cheddad <143810134+farid-cheddad@users.noreply.github.com>
2024-05-23 18:37:24 +0200

    Create TriEnduranceCroissant.java

---
+++ src/TriEnduranceCroissant.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+
+/**
+ * Cette classe implémente l'interface Comparator pour trier les objets Athlete
+ * en fonction de leur endurance de manière croissante
+ */
+public class TriEnduranceCroissant implements Comparator<Athlete> {
+
+    /**
+     * Compare deux objets Athlete en fonction de leur endurance
+     * @param a1 Le premier objet Athlete à comparer
+     * @param a2 Le deuxième objet Athlete à comparer
+     * @return Un entier négatif si l'endurance de a1 est inférieure à celle de a2
+     *         un entier positif si l'endurance de a1 est supérieure à celle de a2
+     *         et zéro si les deux athlètes ont la même endurance
+     */
+    @Override
+    public int compare(Athlete a1, Athlete a2) {
+        int endurance1 = a1.getEndurance();
+        int endurance2 = a2.getEndurance();
+        return endurance1 - endurance2; // Ordre croissant
+    }
+    
+}
