814cf67ba56eb7ae81c41e13dfe1da3e7f5f61f0
elyandreb <143809194+elyandreb@users.noreply.github.com>
2024-06-17 15:34:40 +0200

    Update IUTOAppli.java

--- src/main/java/com/cdal/IUTOAppli.java
+++ src/main/java/com/cdal/IUTOAppli.java
@@ -1,14 +1,18 @@
 package main.java.com.cdal;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -16,63 +20,82 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class IUTOAppli extends Application{
+
+
+public class IUTOAppli extends Application {
     private Scene scene;
     private Button connexion;
     private ImageView logoIUT;
     private ImageView logoIUTO;
     private TextField login;
-    private TextField password;
+    private PasswordField password;
+    private BorderPane borderP;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void init(){
-
+    public void init() {
+        // Initialisation si nécessaire
     }
 
     @Override
     public void start(Stage primaryStage) {
-        this.scene= new Scene(connexion, 500,500);
+        this.borderP = new BorderPane();
+        this.scene = new Scene(borderP, 500, 500);
         primaryStage.setScene(this.scene);
         primaryStage.setTitle("Jeux IUTO");
+        this.pageConnexion();
         primaryStage.show();
     }
 
-    public Pane titre(){
-        HBox titre = new HBox();
+    public Pane titre() {
+        HBox titre = new HBox(10); 
         titre.setBackground(new Background(new BackgroundFill(Color.GREY, null, null)));
+        
+        titre.setPadding(new Insets(10)); 
+
         this.logoIUT = new ImageView(new Image("file:img/logoIUT.png"));
         this.logoIUTO = new ImageView(new Image("file:img/logoIUTO.png"));
         this.connexion = new Button("Connexion");
+        
         titre.getChildren().addAll(this.logoIUT, this.logoIUTO, this.connexion);
         return titre;
-
     }
 
-    public Pane connexion(){
+    public Pane connexion() {
+        VBox VBConnexion = new VBox(10); 
+        VBConnexion.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+         
+        VBConnexion.setPadding(new Insets(20)); 
+
         Label titre1 = new Label("Jeux IUT'Olympiques");
-        VBox VBConnexion = new VBox();
-        connexion.setBackground(new Background(new BackgroundFill(Color.GREY, null, null)));
+        titre1.setFont(new Font("Arial", 30));
+        
+        
         Label titre2 = new Label("Connexion");
         titre2.setFont(new Font("Arial", 20));
+        
 
         this.login = new TextField();
         this.login.setPromptText("Nom d'utilisateur");
-        this.password = new TextField();
+        this.password = new PasswordField();
         this.password.setPromptText("Mot de passe");
 
-        HBox hbBouton = new HBox();
+        HBox hbBouton = new HBox(10); 
+         
+
         Button annuler = new Button("Annuler");
-        Button connexion = new Button(" Se connecter");
-        hbBouton.getChildren().addAll(annuler, connexion);
-        
-        VBConnexion.getChildren().addAll(titre1,titre2, this.login, this.password, hbBouton);
+        Button seConnecter = new Button("Se connecter");
+
+        hbBouton.getChildren().addAll(annuler, seConnecter);
+
+        VBConnexion.getChildren().addAll(titre1, titre2, this.login, this.password, hbBouton);
         return VBConnexion;
     }
 
-
-
-
+    public void pageConnexion() {
+        this.borderP.setCenter(connexion());
+        this.borderP.setTop(titre());
+    }
 }
