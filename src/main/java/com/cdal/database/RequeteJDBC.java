da3ef741424b5fbfbb893ab76ff1e4772953c18b
Nauche <nicolas.nauche@etu.univ-orleans.fr>
2024-06-18 09:38:04 +0200

    creation requeteJDBC + methode classementPays + methode classementEpreuves

---
+++ src/main/java/com/cdal/database/RequeteJDBC.java
@@ -0,0 +1,46 @@
+package src.main.java.com.cdal.database;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class RequeteJDBC {
+    private ConnexionMySql laConnexion;
+    private Statement st;
+    
+    public RequeteJDBC (ConnexionMySql mysql){
+        this.laConnexion = mysql;
+    }
+
+    public ArrayList<String> ClassementPays() throws SQLException {
+        ArrayList<String> listePays = new ArrayList<>();
+        String res  = null;
+        int cpt = 0;
+        this.st = laConnexion.createStatement();
+        ResultSet r = this.st.executeQuery("SELECT nomP, medailleOR, medailleARGENT, medailleBRONZE, " +
+        "(medailleOR + medailleARGENT + medailleBRONZE) as total " +
+        "FROM PAYS " +
+        "ORDER BY totalMedals DESC");
+        while(r.next()){
+            st.close();
+            res += cpt + r.getString(0) + r.getInt(1) + r.getInt(2) + r.getInt(3);
+            cpt+=1;
+            listePays.add(res);
+        }
+        return listePays;
+
+    }
+
+    public ArrayList<String> ClassementEpreuve(String nomE, String sexeA) throws SQLException {
+        int cpt = 0;
+        ArrayList<String>  classement =  new ArrayList<>();
+        String res = null;
+        this.st = LaConnexion.createStatement();
+        ResultSet r = this.st.executeQuery("Select nomEp, prenomA, nomA, nomP, resultats"+" FROM ATHLETE NATURAL JOIN PAYS NATURAL JOIN EPREUVE NATURAL JOIN SCORE"+
+        "where nomEp = "+nomE+"and sexeA = "+sexeA+"ORDER BY resultats");
+        classement.add(r.getString(0));
+        while(r.next()){
+            st.close();
+            res += cpt+ r.getString(1) + r.getString(2) + r.getString(3) + r.getInt(4) ;
+            cpt+=1;
+        }   
+    }
+}
