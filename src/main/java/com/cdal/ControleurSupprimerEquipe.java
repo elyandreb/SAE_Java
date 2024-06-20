312018917af957b09bdb265fe4ba121a45e1437d
KylianDmas <143809037+KylianDmas@users.noreply.github.com>
2024-06-20 22:54:19 +0200

    Update ControleurSupprimerEquipe.java

--- src/main/java/com/cdal/ControleurSupprimerEquipe.java
+++ src/main/java/com/cdal/ControleurSupprimerEquipe.java
@@ -6,14 +6,41 @@ import javafx.event.EventHandler;
 public class ControleurSupprimerEquipe implements EventHandler<ActionEvent> {
     
     private AppIUTO appli;
+    private ConnexionBD connexionBD;
+    private RequeteJDBC requetes;
     
-    public ControleurSupprimerEquipe(AppIUTO appli){
+    public ControleurSupprimerEquipe(AppIUTO appli, ConnexionBD connexionBD, RequeteJDBC requetes){
         this.appli = appli;
+        this.connexionBD = connexionBD;
+        this.requetes = requetes;
     }
     
     @Override
-    public void handle(ActionEvent e){
-        appli.pageSupprimeEquipe();
+    public void handle(ActionEvent e) {
+        String nom = appli.getTextEquipeNom();
+
+        requetes.effacerEquipe(nom);
+
+            // Récupérer la liste actuelle des equipes
+        ObservableList<Equipe> currentList = appli.getTableEquipe().getItems();
+    
+        // Trouver l'équipe avec le nom donné
+       Equipe equipeASupprimer = null;
+        for (Equipe equipe : currentList) {
+            if (equipe.obtenirNom().equals(nom)) {
+                equipeASupprimer = equipe;
+                break;
+            }
+        }
+    
+        // Si l'équipe est trouvé, le supprimer
+        if (equipeASupprimer != null) {
+            currentList.remove(equipeASupprimer);
+            // Rafraîchir le TableView pour refléter les modifications
+            appli.getTableEquipe().refresh();
+        } else {
+            System.err.println("L'équipe avec le nom " + nom + " n'a pas été trouvé.");
+        }
     }
     
 }
