1404931e0aa3bceaa5a9c7e60042b9375d0c1664
elyandreb <143809194+elyandreb@users.noreply.github.com>
2024-06-20 12:32:47 +0200

    Update TriSexeHomme.java

--- src/main/java/com/cdal/TriSexeHomme.java
+++ src/main/java/com/cdal/TriSexeHomme.java
@@ -12,15 +12,15 @@ public class TriSexeHomme implements Comparator<Athlete> {
      * Compare deux objets Athlete en fonction de leur sexe
      * @param a1 Le premier objet Athlete à comparer
      * @param a2 Le deuxième objet Athlete à comparer
-     * @return Un entier négatif si le sexe de a1 est "Homme" et celui de a2 est "Femme"
-     *         un entier positif si le sexe de a1 est "Femme" et celui de a2 est "Homme"
+     * @return Un entier négatif si le sexe de a1 est "H" et celui de a2 est "F"
+     *         un entier positif si le sexe de a1 est "F" et celui de a2 est "H"
      *         et zéro si les deux athlètes ont le même sexe
      */
     @Override
     public int compare(Athlete a1, Athlete a2) {
-        if (a1.obtenirSexe().equals("Homme") && a2.obtenirSexe().equals("Femme")) {
+        if (a1.obtenirSexe().equals("H") && a2.obtenirSexe().equals("F")) {
             return -1;
-        } else if (a1.obtenirSexe().equals("Femme") && a2.obtenirSexe().equals("Homme")) {
+        } else if (a1.obtenirSexe().equals("F") && a2.obtenirSexe().equals("H")) {
             return 1;
         } else {
             return a1.obtenirSexe().compareTo(a2.obtenirSexe());
