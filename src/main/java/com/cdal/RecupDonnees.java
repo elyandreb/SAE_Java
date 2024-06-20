65b8d846ba131c4f172a20d30f9b2169c1d692e5
Kylian <kylian.dumas@etu.univ-orleans.fr>
2024-06-20 10:37:25 +0200

    avancement recupdonnees

--- src/main/java/com/cdal/RecupDonnees.java
+++ src/main/java/com/cdal/RecupDonnees.java
@@ -19,11 +19,12 @@ public class RecupDonnees {
         this.manager = manager;
     }
 
-    @SuppressWarnings("unchecked")
     public void chargerCSV(String nomdufichier) throws FileNotFoundException{
         Scanner scanner = new Scanner(new File(nomdufichier));
         scanner.nextLine();
         HashMap<Epreuve, List<Athlete>> dico = new HashMap<>();
+        Sport sportt = null;
+        Epreuve epr = null;
         while(scanner.hasNextLine()){
             String[] data = scanner.nextLine().split(",");
             String nom = data[0];
@@ -39,13 +40,12 @@ public class RecupDonnees {
             int agilite = Integer.parseInt(data[10]);
             Athlete ath = new Athlete(nom, prenom, sexe, force, agilite, endurance);
             Pays paysm = new Pays(pays, 0,0,0);
-            Sport sportt = null;
             if(sport.charAt(0)=='A'){sportt = new Athletisme(sport, categorie, nbJoueurs);}
             if(sport.charAt(0)=='E'){sportt = new Escrime(sport, categorie, nbJoueurs);}
             if(sport.charAt(0)=='H'){sportt = new Handball(sport, categorie, nbJoueurs);}
             if(sport.charAt(0)=='N'){sportt = new Natation(sport, categorie, nbJoueurs);}
             if(sport.charAt(0)=='V'){sportt = new Volleyball(sport, categorie, nbJoueurs);}
-            Epreuve epr = new Epreuve(epreuve, categorie, sportt);
+            epr = new Epreuve(epreuve, categorie, sportt);
             this.manager.addAthlete(ath);
             this.manager.addPays(paysm);
             this.manager.addSport(sportt);
@@ -54,7 +54,63 @@ public class RecupDonnees {
                 l=new ArrayList<Athlete>();
                 dico.put(epr, l);}
             l.add(ath);
+        }
+        for(Sport spt: this.manager.getSports()){
             this.manager.createEpreuve(dico.get(epr), sportt);
+        }
+        scanner.close();
     }
-}
+
+    public void sauvegarderCSV(String nomdufichier) throws FileNotFoundException{
+        File f = new File(nomdufichier);
+        PrintWriter pw = new PrintWriter(f);
+        Pays p = null;
+        Epreuve e = null;
+        pw.println("Nom,Prénom,Sexe,Pays,Sport,Épreuve,,Catégorie,NombreJoueurs,Force,Endurance,Agilite");
+        for(Athlete a : manager.getAthletes()){
+            for(Pays pays: this.manager.getPays()){
+                if(!(pays.rechercheAthlete(a)==null)){p = pays;}
+            }
+            for(Epreuve epr: this.manager.getEpreuves()){
+                if(!(epr.rechercheParticipantEpreuve(a)==null)){e = epr;}
+            }
+            pw.println(a.obtenirNom()+","+a.getPrenom()+","+a.obtenirSexe()+","+p.getNom()+","+e.getSport()+","+e.getNom()+","+e.getGenre()+","+e.getSport().getNbJoueur()+","+a.getForce()+","+a.getEndurance()+","+a.getAgilite());
+        }
+        pw.close();
+    }
+
+    public void chargerSQl(ConnexionBD connexionBD) throws SQLException{
+        Statement st = connexionBD.createStatement();
+        ResultSet pays = st.executeQuery("SELECT * FROM PAYS");
+
+        while (pays.next()) {
+            manager.addPays(new Pays(pays.getString(1), pays.getInt(2),pays.getInt(3),pays.getInt(4)));
+        }
+
+        ResultSet athletes = st.executeQuery("SELECT * FROM ATHLETE");
+
+        while (athletes.next()) {
+            manager.addAthlete(new Athlete(athletes.getString(2), athletes.getString(3), athletes.getString(4), athletes.getInt(5), athletes.getInt(6), athletes.getInt(7)));
+        }
+
+        ResultSet sports = st.executeQuery("SELECT * FROM SPORT WHERE nombreJoueurs=1");
+
+        while (sports.next()) {
+            if(sports.getString(2).charAt(0)=='A'){manager.addSport(new Athletisme(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='E'){manager.addSport(new Escrime(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='H'){manager.addSport(new Handball(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='N'){manager.addSport(new Natation(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='V'){manager.addSport(new Volleyball(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+        }
+
+        ResultSet sportss = st.executeQuery("SELECT * FROM SPORT WHERE nombreJoueurs!=1");
+
+        while(sportss.next()) {
+            if(sports.getString(2).charAt(0)=='A'){manager.addSport(new Athletisme(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='E'){manager.addSport(new Escrime(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='H'){manager.addSport(new Handball(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='N'){manager.addSport(new Natation(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+            if(sports.getString(2).charAt(0)=='V'){manager.addSport(new Volleyball(sports.getString(2), sports.getString(3), sports.getInt(4)));}
+        }
+    }
 }
