ad8a1680d7993bf8bb2b2fa55f45d8a254314741
KylianDmas <143809037+KylianDmas@users.noreply.github.com>
2024-06-20 21:40:17 +0200

    Update ControleurAjouteAthlete.java

--- src/main/java/com/cdal/ControleurAjouteAthlete.java
+++ src/main/java/com/cdal/ControleurAjouteAthlete.java
@@ -9,13 +9,53 @@ public class ControleurAjouteAthlete implements EventHandler<ActionEvent> {
     private ConnexionBD connexionBD;
     private RequeteJDBC requetes;
     
-    public ControleurAjouteAthlete(AppIUTO appli){
+    public ControleurAjouteAthlete(AppIUTO appli, ConnexionBD connexionBD, RequeteJDBC requetes){
         this.appli = appli;
+        this.connexionBD = connexionBD;
+        this.requetes = requetes;
     }
     
     @Override
-    public void handle(ActionEvent e){
-        
+    public void handle(ActionEvent e) {
+        try {
+            String nom = appli.getTextAthleteNom();
+            String prenom = appli.getTextAthletePrenom();
+            String sexe = appli.getSexeAthlete();
+            int nbAgi = appli.getAgiliteSpinner();
+            int nbEnd = appli.getEnduranceSpinner();
+            int nbFor = appli.getForceSpinner();
+            
+            int choixEquipeR = 10*Math.random();
+            Boolean choixEquipe = null;
+            if(choixEquipeR>=5){choixEquipe = true:}else{choixEquipe = false;}
+            int choixPaysR = 10*Math.Random();
+            
+            Pays pays = null;
+            if(choixPaysR == 0 || choixPaysR == 1 || choixPaysR == 2){pays = new Pays("Maroc", 0,0,0)}
+            else if((choixPaysR == 3 || choixPaysR == 4){pays = new Pays("France", 0,0,0)}
+            else if((choixPaysR == 5 || choixPaysR == 6){pays = new Pays("Allemagne", 0,0,0)}
+            else if((choixPaysR == 7 || choixPaysR == 8){pays = new Pays("Japon", 0,0,0)}
+            else if((choixPaysR == 9 || choixPaysR == 10){pays = new Pays("Corée", 0,0,0)}
+            
+            Athlete athlete = new Athlete(nom, prenom, sexe, nbFor, nbAgi, nbEnd, pays, choixEquipe)
+
+            requetes.ajoutAthlete(athlete);
+
+            ObservableList<Pays> currentList = appli.getTableAthlete().getItems();
+
+            if (currentList.contains(athlete)) {
+                throw new IllegalArgumentException("L'athlète est déjà présent dans la liste.");
+            } else {
+                currentList.add(athlete); // Ajoute le nouveau pays à la liste existante
+            }
+
+            appli.getTableAthlete().refresh();
+            appli.pageAthleteAdministrateur();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        } catch (IllegalArgumentException e2) {
+            System.err.println(e2.getMessage());
+        }
     }
     
 }
